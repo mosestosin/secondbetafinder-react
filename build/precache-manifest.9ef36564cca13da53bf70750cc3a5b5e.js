922b0ec349a958136bdb08f6599eec33af645bdf
Tosin <tosinmoses384@gmail.com>
2019-10-16 09:04:25 +0100

    addedBuildFolder

---
+++ build/precache-manifest.9ef36564cca13da53bf70750cc3a5b5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd81c28a89334485dcf09dd7c036f3d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc78b88244db719bd24c",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "dbec5de28bc9f5d86464",
+    "url": "/static/js/2.12b3564f.chunk.js"
+  },
+  {
+    "revision": "bc78b88244db719bd24c",
+    "url": "/static/js/main.d8d1b7eb.chunk.js"
+  },
+  {
+    "revision": "0925974309b7994419d1",
+    "url": "/static/js/runtime~main.0d8af233.js"
+  }
+]);
